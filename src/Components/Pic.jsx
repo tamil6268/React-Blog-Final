6e407f14f4b502cd6ef6b1165bec1ec5ee6e3c75
TAMILARASAN S <111070542+tamil6268@users.noreply.github.com>
2023-05-28 23:22:09 +0530

    Update Pic.jsx

--- src/Components/Pic.jsx
+++ src/Components/Pic.jsx
@@ -5,12 +5,12 @@ import { useNavigate } from "react-router-dom";
 const Pic=()=>{
 const navigate = useNavigate();
     const handleNavigation = (id, item) => {// two arg - 1. pathname - 2. state
-        navigate(`/bollywood/${id}`, { state: { item } });
+        navigate(`https://heisenberg-tamil.onrender.com/bollywood/${id}`, { state: { item } });
     };
     const [data1,setData1]=useState([]);
     useEffect(()=>{
         axios
-            .get('http://localhost:3008/home/singlepic')
+            .get('https://heisenberg-tamil.onrender.com/home/singlepic')
             .then((response)=>{
                 setData1(response.data);
                 console.log("Pic ",response.data)
